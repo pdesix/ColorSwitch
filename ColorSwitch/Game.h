49724df862a6e38019219657c5f34cca1e77b5ae
Patrick D <desantowski@gmail.com>
2017-09-29 22:04:36 +0200

    std::shared_ptr instead of raw pointers, visual changes

--- ColorSwitch/Game.h
+++ ColorSwitch/Game.h
@@ -10,9 +10,10 @@ int main();
 class Game
 {
 	std::shared_ptr<sf::RenderWindow> m_window;
-	std::shared_ptr<IState> currentState;
-	std::shared_ptr<IInputController> inputController;
-	void ProcessCode(int returnCode);
+	std::shared_ptr<IState> m_currentState;
+	std::shared_ptr<IInputController> m_inputController;
+	void processCode(int returnCode);
+	void run();
 
 public:
 	Game();
