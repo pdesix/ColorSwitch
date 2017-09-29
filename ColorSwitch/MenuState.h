bcbb1728941c5bbbc9521fb7772e61b83c776a4e
Patrick D <desantowski@gmail.com>
2017-09-29 22:08:08 +0200

    New convention, unnecessary structures removed

--- ColorSwitch/MenuState.h
+++ ColorSwitch/MenuState.h
@@ -6,11 +6,11 @@ class MenuState : public IState
 
 public:
 
-	inline virtual int ManageGraphic(std::shared_ptr<sf::RenderWindow> window) override 
+	inline virtual int manageGraphic(std::shared_ptr<sf::RenderWindow> window) override 
 	{ 
 		window->clear(sf::Color::White); 
 		return -1;
 	}
-	virtual int HandleInput(sf::Event & event) override { return -1 };
-	inline virtual int ProcessLogic(sf::Time deltaTime) override { return Codes::Null; };
+	virtual int handleInput(sf::Event & event) override { return -1 };
+	inline virtual int processLogic(sf::Time deltaTime) override { return Codes::Null; };
 };
