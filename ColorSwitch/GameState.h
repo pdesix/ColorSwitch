48d4108741cf3d453011e0b476bd1601b178a4cf
Patrick D <desantowski@gmail.com>
2017-10-01 15:22:49 +0200

    Added Map object (including logic processing)

--- ColorSwitch/GameState.h
+++ ColorSwitch/GameState.h
@@ -1,19 +1,33 @@
 #pragma once
 #include "Interfaces.h"
 #include "Player.h"
+#include "Map.h"
 
 template<class T>
 class GameState : public IState<T>
 {
 private:
 	std::shared_ptr<Player> m_player;
+	Map<GameState> m_gameMap;
+
 public:
-	GameState(Game & baseGame, GameCallback processFunction) : IState(baseGame, processFunction), m_player{std::make_shared{new Player()}} 
+	GameState(Game & baseGame, GameCallback processFunction) : IState(baseGame, processFunction), m_player{std::make_shared<Player>(Player())}, m_gameMap{m_player,*this,&Player::onColorChange,&GameState::onDeath, &GameState::onPoint}
 	{ 
 		m_drawables.push_back(m_player);
+		std::vector<std::shared_ptr<sf::Drawable>> mapDrawables{ m_gameMap.getDrawables() };
+		for (std::shared_ptr<sf::Drawable> drawable : mapDrawables)
+			m_drawables.push_back(drawable);
 	}
 
-	virtual void handleInput(sf::Event & event) override { return -1; };
-	inline virtual void processLogic(sf::Time deltaTime) override { };
-};
+	void onDeath()
+	{
+		runCallback(GameStart);
+	}
 
+	void onPoint()
+	{
+	}
+
+	virtual void handleInput(sf::Event & event) override { return; };
+	inline virtual void processLogic(sf::Time deltaTime) override { m_gameMap.processLogic(deltaTime); };
+};
