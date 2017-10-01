e6be33487674b357a33d345b9f5dff4ea4875758
Patrick D <desantowski@gmail.com>
2017-10-01 21:05:31 +0200

    Refactoring, bug fix

--- ColorSwitch/MenuScene.hpp
+++ ColorSwitch/MenuScene.hpp
@@ -5,38 +5,25 @@
 template<class Game>
 class MenuScene : public BaseScene<Game>
 {
+	std::unique_ptr<sf::Font> m_font;
 public:
-	MenuScene(Game & baseGame, GameCallback processFunction) : BaseScene(baseGame, processFunction)
+	MenuScene(Game & baseGame, GameCallback processFunction) : BaseScene(baseGame, processFunction), m_font{new sf::Font()}
 	{
-		//std::shared_ptr<sf::Text>(play);
-		//std::shared_ptr<sf::Text>(exit);
-		sf::Text play;
-		sf::Text exit;
-		sf::Font font;
+		std::shared_ptr<sf::Text> play{ new sf::Text("Play", *m_font, 60u) };
+		std::shared_ptr<sf::Text> exit{ new sf::Text("Exit", *m_font, 60u) };
 
-		if (!font.loadFromFile("assets/arial.ttf")) {
+		if (!m_font->loadFromFile("assets/arial.ttf")) {
 			std::cerr << "cannot load assets/arial.ttf";
 		}
 
-		std::array<std::string, 2> words = { "Play", "Exit" };
+		play->setFillColor(sf::Color::Black);
+		exit->setFillColor(sf::Color::Black);
 
-		play.setFont(font);
-		exit.setFont(font);
-
-		play.setString(words[0]);
-		exit.setString(words[1]);
-
-		play.setCharacterSize(60);
-		exit.setCharacterSize(60);
-
-		play.setFillColor(sf::Color::Black);
-		exit.setFillColor(sf::Color::Black);
-
-		play.setPosition(800 / 2, 20);
-		exit.setPosition(800 / 2, 200);
+		play->setPosition(800 / 2, 20);
+		exit->setPosition(800 / 2, 200);
 		
-		m_drawables.push_back(std::make_shared<sf::Text>(play));
-		m_drawables.push_back(std::make_shared<sf::Text>(exit));
+		m_drawables.push_back(play);
+		m_drawables.push_back(exit);
 	}
 
 	virtual void handleInput(sf::Event & event) override
