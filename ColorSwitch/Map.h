55743166886eb50ac18df07a30c8b72ed12aaaa7
Patrick D <desantowski@gmail.com>
2017-10-01 16:11:26 +0200

    Circle texture moved to Map class

--- ColorSwitch/Map.h
+++ ColorSwitch/Map.h
@@ -13,21 +13,16 @@ class Obstacle : public sf::CircleShape
 	std::shared_ptr<Player> m_player;
 	GameStateFunction m_playerDeath;
 
-	sf::Texture* m_circleTexture;
-
 public:
-	Obstacle(int i, std::shared_ptr<Player> gamePlayer, GameState & baseObject, GameStateFunction playerDeathCallback) : m_base{ baseObject }, m_player { gamePlayer }, m_playerDeath{ playerDeathCallback }, m_intern{std::make_shared<sf::CircleShape>()}, m_circleTexture{new sf::Texture()}
+	Obstacle(int positionIndex, std::shared_ptr<Player> gamePlayer, GameState & baseObject, GameStateFunction playerDeathCallback) : m_base{ baseObject }, m_player { gamePlayer }, m_playerDeath{ playerDeathCallback }, m_intern{std::make_shared<sf::CircleShape>()}
 	{
-		if (!m_circleTexture->loadFromFile("assets/data001.png")) std::cerr << "cannot load assets/data001.png";
-		setTexture(m_circleTexture,true);
-		
 		m_intern->setFillColor(sf::Color::Red);
 		setFillColor(sf::Color::Black);
 
 		setRadius(25.f);
 		m_intern->setRadius(20.f);
 
-		setPosition(400.f - (getGlobalBounds().width / 2), -150.f * i);
+		setPosition(400.f - (getGlobalBounds().width / 2), -150.f * positionIndex);
 		m_intern->setPosition(this->getPosition().x - m_intern->getGlobalBounds().height/8, this->getPosition().y + m_intern->getGlobalBounds().width/8);
 	}
 
@@ -38,9 +33,6 @@ public:
 		setPosition(getPosition().x, getPosition().y + 5.f);
 		m_intern->setPosition(m_intern->getPosition().x, m_intern->getPosition().y + 5.f);
 		
-		std::cout << "Pos: " << getPosition().x << " ; " << getPosition().y << " \n";
-		std::cout << "\t intern pos: " << m_intern->getPosition().x << " ; " << m_intern->getPosition().y << " \n";
-
 		rotate(1.f);
 		m_intern->rotate(1.f);
 
@@ -80,23 +72,27 @@ class Map : public ILogicProcessor
 	std::vector<std::shared_ptr<sf::Drawable>> m_drawables;
 	std::vector<std::shared_ptr<Obstacle<GameState>>> m_obstacles;
 
+	std::shared_ptr<sf::Texture> m_circleTexture;
+
 public:
-	Map(std::shared_ptr<Player> playerPointer, GameState & gameController, PlayerFunction playerCollorChangedCallback, GameStateFunction deathCallback, GameStateFunction pointCallback)
-		: m_base{ gameController }, m_playerDeathCallback { deathCallback }, m_pointGainedCallback{ pointCallback }, m_player{ playerPointer }, m_playerCollorChanged{ playerCollorChangedCallback }
+	Map(std::vector<std::shared_ptr<sf::Drawable>> & drawables, std::shared_ptr<Player> playerPointer, GameState & gameController, PlayerFunction playerCollorChangedCallback, GameStateFunction deathCallback, GameStateFunction pointCallback)
+		: m_base{ gameController }, m_playerDeathCallback { deathCallback }, m_pointGainedCallback{ pointCallback }, m_player{ playerPointer }, m_playerCollorChanged{ playerCollorChangedCallback }, m_circleTexture{ std::make_shared<sf::Texture>(sf::Texture()) }
 	{
+		if (!m_circleTexture->loadFromFile("assets/data001.png")) std::cerr << "cannot load assets/data001.png";
 		for (int i = 0; i < 1; i++)
 		{
 			std::shared_ptr<Obstacle<GameState>> obstacle{ std::make_shared<Obstacle<GameState>>(i, playerPointer, gameController, deathCallback) };
 
-			m_drawables.push_back(obstacle);
+	 		obstacle->setTexture(&(*m_circleTexture), true);
+
 			m_obstacles.push_back(obstacle);
 
-			m_drawables.push_back(obstacle->getInternalDrawable());
+			drawables.push_back(obstacle);
+			drawables.push_back(obstacle->getInternalDrawable());
 		}
 	}
 
 	~Map() { }
-	std::vector<std::shared_ptr<sf::Drawable>> getDrawables() { return m_drawables; }
 
 	virtual void processLogic(sf::Time deltaTime) override
 	{
