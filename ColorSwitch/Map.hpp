07378491a0a0c59828e30155142da150f502c328
Patrick D <desantowski@gmail.com>
2017-10-01 22:10:59 +0200

    Movement and rotation of obstacles

--- ColorSwitch/Map.hpp
+++ ColorSwitch/Map.hpp
@@ -25,7 +25,7 @@ public:
 		setOrigin(getGlobalBounds().height / 2.f, getGlobalBounds().width / 2.f);
 		m_intern->setOrigin(m_intern->getGlobalBounds().height / 2.f, m_intern->getGlobalBounds().width / 2.f);
 
-		setPosition(400.f, -150.f * positionIndex);
+		setPosition(400.f, -350.f * positionIndex);
 
 		float pX = getGlobalBounds().left + getGlobalBounds().width / 2.f;
 		float pY = getGlobalBounds().top + getGlobalBounds().height / 2.f;
@@ -34,13 +34,16 @@ public:
 
 	std::shared_ptr<sf::CircleShape> getInternalDrawable() { return m_intern; }
 
-	void move()
+	void rotate(sf::Time deltaTime)
 	{
-		setPosition(getPosition().x, getPosition().y + 5.f);
-		m_intern->setPosition(m_intern->getPosition().x, m_intern->getPosition().y + 5.f);
-		
-		rotate(1.f);
-		m_intern->rotate(1.f);
+		sf::CircleShape::rotate(60.f * deltaTime.asSeconds());
+		m_intern->rotate(60.f * deltaTime.asSeconds());
+	}
+
+	void move(sf::Time deltaTime)
+	{
+		setPosition(getPosition().x, getPosition().y + 60.f * deltaTime.asSeconds());
+		m_intern->setPosition(m_intern->getPosition().x, m_intern->getPosition().y + 60.f * deltaTime.asSeconds());
 
 		while (getPosition().y > 750.f) setPosition(getPosition().x, getPosition().y - 750.f);
 		while (m_intern->getPosition().y > 750.f) m_intern->setPosition(m_intern->getPosition().x, m_intern->getPosition().y - 750.f);
@@ -48,17 +51,16 @@ public:
 	
 	void checkCollision()
 	{
-		if (m_player->getGlobalBounds().intersects(sf::Sprite::getGlobalBounds()))
+		if (m_player->getGlobalBounds().intersects(sf::CircleShape::getGlobalBounds()))
 		{
-			auto color{ []
+			auto color = [=]
 			{
 				float rotation{ sf::Sprite::getRotation() };
-				if (static_cast<int>{rotation} % 90 < 10) return -1;
-				else return static_cast<int>{rotation / 90.f};
-			}(); };
-			if (m_player->getColor() != color && m_player->getGlobalBounds().intersects(m_intern->getGlobalBounds()) == false) { return m_base->m_playerDeath(); }
+				if (static_cast<int>(rotation) % 90 < 10) return -1;
+				else return static_cast<int>(rotation / 90.f);
+			}();
+			if (m_player->getColor() != color && m_player->getGlobalBounds().intersects(m_intern->getGlobalBounds()) == false) { return m_playerDeath(m_base); }
 		}
-
 	}
 };
 
@@ -84,7 +86,7 @@ public:
 		m_pointGainedCallback{ &GameScene::onPoint }, m_playerCollorChanged{ &Player::onColorChange }
 	{
 		if (!m_circleTexture->loadFromFile("assets/data001.png")) std::cerr << "cannot load assets/data001.png";
-		for (int i = 0; i < 1; i++)
+		for (int i = 0; i < 2; i++)
 		{
 			std::shared_ptr<Obstacle<GameScene>> obstacle{ new Obstacle<GameScene>(i, playerPointer, gameController) };
 
@@ -97,10 +99,19 @@ public:
 		}
 	}
 
+	void move(sf::Time deltaTime)
+	{
+		for (std::shared_ptr<Obstacle<GameScene>> obstacle : m_obstacles)
+			obstacle->move(deltaTime);
+	}
+
 	virtual void processLogic(sf::Time deltaTime) override
 	{
 		for (std::shared_ptr<Obstacle<GameScene>> obstacle : m_obstacles)
-			obstacle->move();
+		{
+			obstacle->rotate(deltaTime);
+			obstacle->checkCollision();
+		}
 	}
 };
 
