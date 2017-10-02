5ed629532affeee5453e45d68295b6adf76b61b5
Erzis900 <Erzis900@gmail.com>
2017-10-02 20:51:04 +0300

    Added score + fixes

--- ColorSwitch/MenuScene.hpp
+++ ColorSwitch/MenuScene.hpp
@@ -9,6 +9,7 @@ class MenuScene : public BaseScene<Game>
 	sf::Texture menu_texture;
 
 	std::unique_ptr<sf::Font> m_font;
+	std::shared_ptr<sf::Text> stars;
 	std::shared_ptr<sf::Text> play;
 	std::shared_ptr<sf::Text> shop;
 	std::shared_ptr<sf::Text> exit;
@@ -22,6 +23,7 @@ public:
 	MenuScene(Game & baseGame, GameCallback processFunction)
 		: BaseScene(baseGame, processFunction), m_font{ new sf::Font() }, 
 		m_lastClick{ std::chrono::system_clock::now() },
+		stars{ new sf::Text("Stars: 0", *m_font, 45u) },
 		play{ new sf::Text("Play", *m_font, 100u) },
 		shop{ new sf::Text("Shop", *m_font, 60u) },
 		exit{ new sf::Text("Exit", *m_font, 60u) },
@@ -34,11 +36,13 @@ public:
 
 		menu_bg->setTexture(menu_texture);
 
+		stars->setFillColor(sf::Color::White);
 		play->setFillColor(sf::Color::White);
 		shop->setFillColor(sf::Color::White);
 		exit->setFillColor(sf::Color::White);
 		authors->setFillColor(sf::Color::White);
 
+		stars->setPosition(20, 20);
 		play->setPosition(800 / 2 - 60, 100);
 		shop->setPosition(800 / 2 - 60, 250);
 		exit->setPosition(800 / 2 - 60, 350);
@@ -46,6 +50,7 @@ public:
 
 
 		m_drawables.push_back(menu_bg);
+		m_drawables.push_back(stars);
 		m_drawables.push_back(play);
 		m_drawables.push_back(shop);
 		m_drawables.push_back(exit);
@@ -87,6 +92,8 @@ public:
 			if (elapsed.count() > 0.25) m_lastClick = now;
 			else return;
 			switchOption(++selected);
+			if (selected > 3) selected = 3;
+			else if (selected < 1) selected = 1;
 		}
 		else if (event.KeyPressed && event.key.code == sf::Keyboard::Up) {
 			std::chrono::system_clock::time_point now{ std::chrono::system_clock::now() };
@@ -94,10 +101,15 @@ public:
 			if (elapsed.count() > 0.25) m_lastClick = now;
 			else return;
 			switchOption (--selected);
+			if (selected > 3) selected = 3;
+			else if (selected < 1) selected = 1;
 		}
 		else if (event.KeyPressed && event.key.code == sf::Keyboard::Return) {
 			if (selected == 1) {
 				runCallback(LoopCodes::GameStart);
+			}
+			else if (selected == 2) {
+				//runCallback(LoopCodes::Shop);
 			}
 			else if(selected == 3){
 				runCallback(LoopCodes::GameClose);
